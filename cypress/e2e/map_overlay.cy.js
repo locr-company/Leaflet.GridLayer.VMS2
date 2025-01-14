8e4863db690accc4bc801a3570e0bb4ea9d99126
ringostarr80 <ringo19@gmx.de>
2025-01-14 20:19:20 +0100

    Füge Bildvergleichsfunktionalität in Cypress-Tests hinzu und aktualisiere .gitignore

--- cypress/e2e/map_overlay.cy.js
+++ cypress/e2e/map_overlay.cy.js
@@ -1,32 +1,9 @@
 describe('map overlay specs', () => {
   const prefix = 'data:image/png;base64,'
 
-  /*
   it('can display an empty map with a SvgLayer.', () => {
-    cy.visit('http://localhost:9876/map_overlay_with_svg_layer.html')
-
-    cy.window().then(win => {
-      const map = win.document.getElementById('map')
-      if (map.leafletMap instanceof win.L.Map) {
-        map.leafletMap.eachLayer(layer => {
-          if (!(layer instanceof win.L.GridLayer.VMS2)) {
-            return
-          }
-
-          cy.wrap(layer.getPrintCanvas()).then(canvas => {
-            const image = canvas[0].toDataURL('image/png')
-            cy.fixture('map_overlay_with_svg_layer.png').then(refImage => {
-              expect(refImage).to.equal(image.slice(prefix.length))
-            })
-          })
-        })
-      }
-    })
-  })
-  //*/
-
-  it('can display an empty map with a TextSvgLayer.', () => {
-    cy.visit('http://localhost:9876/map_overlay_with_text_svg_layer.html')
+    const baseFilename = 'map_overlay_with_svg_layer'
+    cy.visit(`http://localhost:9876/${baseFilename}.html`)
 
     cy.window().then(win => {
       const map = win.document.getElementById('map')
@@ -39,9 +16,13 @@ describe('map overlay specs', () => {
           cy.wrap(layer.getPrintCanvas()).then(canvas => {
             const image = canvas[0].toDataURL('image/png')
             const base64Data = image.slice(prefix.length);
-            cy.writeFile('cypress/artifacts/map_overlay_with_text_svg_layer.png', base64Data, 'base64');
-            cy.fixture('map_overlay_with_text_svg_layer.png').then(refImage => {
-              expect(refImage).to.equal(base64Data)
+            cy.writeFile(`cypress/artifacts/${baseFilename}.png`, base64Data, 'base64');
+            cy.task('comparePngs', {
+              base: `cypress/fixtures/${baseFilename}.png`,
+              compare: `cypress/artifacts/${baseFilename}.png`,
+              diffPath: `cypress/artifacts/${baseFilename}-diff.png`,
+            }).then((diffPixelCount) => {
+              expect(diffPixelCount).to.equal(0)
             })
           })
         })
@@ -49,9 +30,9 @@ describe('map overlay specs', () => {
     })
   })
 
-  /*
-  it('can display an empty map with an ImageSvgLayer.', () => {
-    cy.visit('http://localhost:9876/map_overlay_with_image_svg_layer.html')
+  it('can display an empty map with a TextSvgLayer.', () => {
+    const baseFilename = 'map_overlay_with_text_svg_layer'
+    cy.visit(`http://localhost:9876/${baseFilename}.html`)
 
     cy.window().then(win => {
       const map = win.document.getElementById('map')
@@ -63,8 +44,43 @@ describe('map overlay specs', () => {
 
           cy.wrap(layer.getPrintCanvas()).then(canvas => {
             const image = canvas[0].toDataURL('image/png')
-            cy.fixture('map_overlay_with_image_svg_layer.png').then(refImage => {
-              expect(refImage).to.equal(image.slice(prefix.length))
+            const base64Data = image.slice(prefix.length);
+            cy.writeFile(`cypress/artifacts/${baseFilename}.png`, base64Data, 'base64');
+            cy.task('comparePngs', {
+              base: `cypress/fixtures/${baseFilename}.png`,
+              compare: `cypress/artifacts/${baseFilename}.png`,
+              diffPath: `cypress/artifacts/${baseFilename}-diff.png`,
+            }).then((diffPixelCount) => {
+              expect(diffPixelCount).to.equal(0)
+            })
+          })
+        })
+      }
+    })
+  })
+
+  it('can display an empty map with an ImageSvgLayer.', () => {
+    const baseFilename = 'map_overlay_with_image_svg_layer'
+    cy.visit(`http://localhost:9876/${baseFilename}.html`)
+
+    cy.window().then(win => {
+      const map = win.document.getElementById('map')
+      if (map.leafletMap instanceof win.L.Map) {
+        map.leafletMap.eachLayer(layer => {
+          if (!(layer instanceof win.L.GridLayer.VMS2)) {
+            return
+          }
+
+          cy.wrap(layer.getPrintCanvas()).then(canvas => {
+            const image = canvas[0].toDataURL('image/png')
+            const base64Data = image.slice(prefix.length);
+            cy.writeFile(`cypress/artifacts/${baseFilename}.png`, base64Data, 'base64');
+            cy.task('comparePngs', {
+              base: `cypress/fixtures/${baseFilename}.png`,
+              compare: `cypress/artifacts/${baseFilename}.png`,
+              diffPath: `cypress/artifacts/${baseFilename}-diff.png`,
+            }).then((diffPixelCount) => {
+              expect(diffPixelCount).to.equal(0)
             })
           })
         })
@@ -73,7 +89,8 @@ describe('map overlay specs', () => {
   })
 
   it('can display an empty map with an ImageSvgLayer with a relative image path.', () => {
-    cy.visit('http://localhost:9876/map_overlay_with_relative_image_svg_layer.html')
+    const baseFilename = 'map_overlay_with_relative_image_svg_layer'
+    cy.visit(`http://localhost:9876/${baseFilename}.html`)
 
     cy.window().then(win => {
       const map = win.document.getElementById('map')
@@ -85,8 +102,14 @@ describe('map overlay specs', () => {
 
           cy.wrap(layer.getPrintCanvas()).then(canvas => {
             const image = canvas[0].toDataURL('image/png')
-            cy.fixture('map_overlay_with_image_svg_layer.png').then(refImage => {
-              expect(refImage).to.equal(image.slice(prefix.length))
+            const base64Data = image.slice(prefix.length);
+            cy.writeFile(`cypress/artifacts/${baseFilename}.png`, base64Data, 'base64');
+            cy.task('comparePngs', {
+              base: `cypress/fixtures/${baseFilename}.png`,
+              compare: `cypress/artifacts/${baseFilename}.png`,
+              diffPath: `cypress/artifacts/${baseFilename}-diff.png`,
+            }).then((diffPixelCount) => {
+              expect(diffPixelCount).to.equal(0)
             })
           })
         })
@@ -95,7 +118,8 @@ describe('map overlay specs', () => {
   })
 
   it('can display an empty map with a PoiLayer.', () => {
-    cy.visit('http://localhost:9876/map_overlay_with_poi_layer.html')
+    const baseFilename = 'map_overlay_with_poi_layer'
+    cy.visit(`http://localhost:9876/${baseFilename}.html`)
 
     cy.window().then(win => {
       const map = win.document.getElementById('map')
@@ -107,13 +131,18 @@ describe('map overlay specs', () => {
 
           cy.wrap(layer.getPrintCanvas()).then(canvas => {
             const image = canvas[0].toDataURL('image/png')
-            cy.fixture('map_overlay_with_poi_layer.png').then(refImage => {
-              expect(refImage).to.equal(image.slice(prefix.length))
+            const base64Data = image.slice(prefix.length);
+            cy.writeFile(`cypress/artifacts/${baseFilename}.png`, base64Data, 'base64');
+            cy.task('comparePngs', {
+              base: `cypress/fixtures/${baseFilename}.png`,
+              compare: `cypress/artifacts/${baseFilename}.png`,
+              diffPath: `cypress/artifacts/${baseFilename}-diff.png`,
+            }).then((diffPixelCount) => {
+              expect(diffPixelCount).to.equal(0)
             })
           })
         })
       }
     })
   })
-  //*/
 })
