8136f9e14144af89d0c996b336525ed7594fb3f3
Ringo Leese <r.leese@locr.com>
2025-02-25 15:25:44 +0100

    Refactor map display test to use a reusable function and save comparison images

--- cypress/e2e/default_map.cy.js
+++ cypress/e2e/default_map.cy.js
@@ -1,25 +1,36 @@
 describe('basic map specs', () => {
   const prefix = 'data:image/png;base64,'
-
-  it('can display a default map of locr HQ in BS.', () => {
-    cy.visit('http://localhost:9876/default_map.html')
+  const visitAndCompare = (baseFilename) => {
+    cy.visit(`http://localhost:9876/${baseFilename}.html`)
 
     cy.window().then(win => {
       const map = win.document.getElementById('map')
-      if (map.leafletMap instanceof win.L.Map) {
-        map.leafletMap.eachLayer(layer => {
-          if (!(layer instanceof win.L.GridLayer.VMS2)) {
-            return
-          }
+      if (!(map.leafletMap instanceof win.L.Map)) {
+        return
+      }
 
-          cy.wrap(layer.getPrintCanvas()).then(canvas => {
-            const image = canvas[0].toDataURL('image/png')
-            cy.fixture('default_map.png').then(refImage => {
-              expect(refImage).to.equal(image.slice(prefix.length))
-            })
+      map.leafletMap.eachLayer(layer => {
+        if (!(layer instanceof win.L.GridLayer.VMS2)) {
+          return
+        }
+
+        cy.wrap(layer.getPrintCanvas()).then(canvas => {
+          const image = canvas[0].toDataURL('image/png')
+          const base64Data = image.slice(prefix.length)
+          cy.writeFile(`cypress/artifacts/${baseFilename}.png`, base64Data, 'base64')
+          cy.task('comparePngs', {
+            base: `cypress/fixtures/${baseFilename}.png`,
+            compare: `cypress/artifacts/${baseFilename}.png`,
+            diffPath: `cypress/artifacts/${baseFilename}-diff.png`,
+          }).then((diffPixelCount) => {
+            expect(diffPixelCount).to.lessThan(100)
           })
         })
-      }
+      })
     })
+  }
+
+  it('can display a default map of locr HQ in BS.', () => {
+    visitAndCompare('default_map')
   })
 })
